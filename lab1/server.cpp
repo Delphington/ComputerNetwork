eb13515a884ac826002586073a61757e5359e753
Delphington <89201997+Delphington@users.noreply.github.com>
2026-04-01 14:28:35 +0300

    lab2

--- lab1/server.cpp
+++ lab1/server.cpp
@@ -4,37 +4,33 @@
 #include <unistd.h>
 
 int main() {
-    int sockfd;                         // Дескриптор сокета
-    char buffer[1024];                   // Буфер для приема данных
-    struct sockaddr_in servaddr, cliaddr; // Структуры адресов сервера и клиента
+    int sockfd;
+    char buffer[1024];
+    struct sockaddr_in servaddr, cliaddr;
 
-    // Создание UDP сокета
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
     // Настройка адреса сервера
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;       // IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY; // Принимать соединения на всех интерфейсах
-    servaddr.sin_port = htons(8080);      // Порт 8080
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(8080);
 
-    // Привязка сокета к адресу
     bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
 
     std::cout << "Сервер запущен и ожидает сообщений..." << std::endl;
 
     while (true) {
-        socklen_t len = sizeof(cliaddr);   // Размер структуры клиента
-        // Получение данных от клиента
-        int n = recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr *)&cliaddr, &len);
-        buffer[n] = '\0';                   // Завершающий нуль для строки
+        socklen_t len = sizeof(cliaddr);
 
-        // Вывод сообщения с адресом клиента
+        int n = recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr *)&cliaddr, &len);
+        buffer[n] = '\0';
+
         std::cout << "Клиент [" << inet_ntoa(cliaddr.sin_addr) << "]: " << buffer << std::endl;
 
-        // Эхо-ответ
         sendto(sockfd, buffer, n, 0, (struct sockaddr *)&cliaddr, len);
     }
 
-    close(sockfd);                         // Закрытие сокета
+    close(sockfd);
     return 0;
 }
